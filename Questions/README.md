9c5727b6f9973c919d07697f231dbd2585b45ca2
Aycan Aleyna Yerdelen <70997613+ayerdelen@users.noreply.github.com>
2021-01-22 09:31:38 +0300

    Questions are done

---
+++ Questions/README.md
@@ -0,0 +1,12 @@
+# Questions
+
+[Click to see](https://ayerdelen.github.io/basic-projects/Questions)
+
+- This is a tutorial of [Free Code Camp](https://www.youtube.com/watch?v=3PHXvlpOkf4&list=WL&index=21&t=21s)
+
+## I've learned:
+-Selecting multiple querySelector into the elements,
+-toggle classList,
+-Open one section and closing the other ones.
+
+![.](ss.png)
--- Questions/app.js
+++ Questions/app.js
@@ -1,2 +1,18 @@
 //using selectors inside the element
 // traversing the dom
+const qText = document.querySelectorAll(".question");
+
+qText.forEach((question) => {
+  const btn = question.querySelector(".question-btn");
+
+  btn.addEventListener("click", () => {
+    //open just one question
+    qText.forEach((item) => {
+      if (item !== question) {
+        item.classList.remove("show-text");
+      }
+    });
+
+    question.classList.toggle("show-text");
+  });
+});
